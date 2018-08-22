0e660c4b50d53259aabd420e19b1aedc28a91d66
Adam Schellenberg <adamschellenberg93@gmail.com>
2018-08-21 21:50:16 -0700

    started paddle controller modification
    began to modify script to toggle which paddle is active dependent on location of ball

--- Pong/Assets/Assets/Script/PlayerController.cs
+++ Pong/Assets/Assets/Script/PlayerController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	[SerializeField]
+	GameObject leftPaddle;
+	[SerializeField]
+	GameObject rightPaddle;
+	[SerializeField]
+	Collider2D detectBallCollider;
+
 	public float moveSpeed;
 
-	Rigidbody2D rb;
+	[SerializeField]
+	Rigidbody2D leftPaddleRigidBody;
+	[SerializeField]
+	Rigidbody2D rightPaddleRigidBody;
 
 	// Use this for initialization
 	void Start () {
-		rb = gameObject.GetComponent<Rigidbody2D>();
 
 	}
 	
@@ -19,20 +28,42 @@ public class PlayerController : MonoBehaviour {
 
 		// Default speed to 0 if button not pressed
 
-		rb.velocity = new Vector2 (0f, 0f);
+		//rb.velocity = new Vector2 (0f, 0f);
 
 		if (Input.GetKey ("up")) {
 			//move player up
-			if (transform.position.y < 4.2) {
-				rb.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, moveSpeed);
-			}
+			MovePaddleUp();
 
 		} else if (Input.GetKey ("down")) {
 			//move player down
-			if (transform.position.y > -4.2) {
-				rb.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, moveSpeed * -1);
-			}
+			MovePaddleDown();
+		}
+			
+	}
+
+	void MovePaddleUp() {
+
+		if (transform.position.y < 4.2) {
+			//rb.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, moveSpeed);
+			// Move the paddle that is active up
 		}
 
 	}
+
+	void MovePaddleDown() {
+
+		if (transform.position.y > -4.2) {
+			//rb.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, moveSpeed * -1);
+			// Move the paddle that is active down
+		}
+
+	}
+		
+	void OnTriggerStay2D(Collider2D other) {
+	
+		print ("test");
+		// Toggle active paddle
+
+	}
+
 }
